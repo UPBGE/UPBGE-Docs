d7f3a0deb168fa2c782ac0caf8881ee9c29455c2
Jorge Bernal <jbernalmartinez@gmail.com>
2021-01-20 00:10:24 +0100

    Adapt conf.py to Blender one

--- source/conf.py
+++ source/conf.py
@@ -33,12 +33,17 @@ release = 'latest'
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = '1.0'
+needs_sphinx = '1.5'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    #'youtube',
+    #'vimeo',
+    #'sphinx.ext.mathjax',
+    #'sphinx.ext.intersphinx',
+    #'sphinx.ext.todo',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -63,10 +68,10 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = []
+exclude_patterns = ['_build']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'default'
 highlight_language = 'python3'
 
 # -- Options for HTML output -------------------------------------------------
@@ -74,10 +79,16 @@ highlight_language = 'python3'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "default"
+try:
+    import sphinx_rtd_theme
+    html_theme = "sphinx_rtd_theme"
+    del sphinx_rtd_theme
+except ModuleNotFoundError:
+    pass
 
 # Theme options
-html_theme_options = {
+#html_theme_options = {
     # 'typekit_id': 'hiw1hhg',
     # 'analytics_id': '',
     # 'sticky_navigation': True  # Set to False to disable the sticky nav while scrolling.
@@ -85,7 +96,7 @@ html_theme_options = {
     #'collapse_navigation': False,  # Collapse navigation (False makes it tree-like)
     # 'display_version': True,  # Display the docs version
     # 'navigation_depth': 4,  # Depth of the headers shown in the navigation bar
-}
+#}
 
 html_logo = 'images/upbge_logo.png'
 
@@ -93,7 +104,23 @@ html_logo = 'images/upbge_logo.png'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {}
+
+# Add any paths that contain custom themes here, relative to this directory.
+html_theme_path = []
+
+if html_theme == "sphinx_rtd_theme":
+    html_theme_options = {
+        # included in the title
+        "display_version": False,
+        "collapse_navigation": True,
+        "navigation_depth": -1,
+        "canonical_url": "https://upbge.org/manual",
+    }
+
+    extensions.append('sphinx_rtd_theme')
+
+html_title = "UPBGE Manual"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -114,7 +141,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'UPBGEManualdoc'
+htmlhelp_basename = 'UPBGE Reference Manual'
 
 
 # -- Options for LaTeX output ------------------------------------------------
