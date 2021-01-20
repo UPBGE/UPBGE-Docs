fd077fd9643ee7aae1223f6d08293038e63f858a
lordloki <jbernalmartinez@gmail.com>
2021-01-20 22:13:36 +0100

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -43,12 +43,10 @@ needs_sphinx = '1.5'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-import sphinx_rtd_theme
 
 extensions = [
     'youtube',
     'vimeo',
-    #'sphinx_rtd_theme',
     #'sphinx.ext.mathjax',
     #'sphinx.ext.intersphinx',
     #'sphinx.ext.todo',
@@ -93,6 +91,8 @@ html_title = 'UPBGE Manual'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+import sphinx_rtd_theme
+
 html_theme = 'sphinx_rtd_theme'
 
 # Theme options
@@ -101,7 +101,7 @@ html_theme_options = {
         "display_version": False,
         "collapse_navigation": True,
         "navigation_depth": -1,
-        "canonical_url": "https://upbge.org/manual",
+        "html_baseurl": "https://upbge.org/manual",
     }
 
 extensions.append('sphinx_rtd_theme')
@@ -109,7 +109,7 @@ extensions.append('sphinx_rtd_theme')
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
