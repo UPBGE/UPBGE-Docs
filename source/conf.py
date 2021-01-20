06adf35efe19f631e2c67833d201150c61d64222
lordloki <jbernalmartinez@gmail.com>
2021-01-20 18:06:27 +0100

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -42,9 +42,12 @@ needs_sphinx = '1.5'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+import sphinx_rtd_theme
+
 extensions = [
     'youtube',
     'vimeo',
+    'sphinx_rtd_theme',
     #'sphinx.ext.mathjax',
     #'sphinx.ext.intersphinx',
     #'sphinx.ext.todo',
@@ -75,56 +78,31 @@ language = 'en'
 exclude_patterns = ['_build']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'default'
+pygments_style = 'sphynx'
 highlight_language = 'python3'
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "default"
-try:
-    import sphinx_rtd_theme
-    html_theme = "sphinx_rtd_theme"
-    del sphinx_rtd_theme
-except ModuleNotFoundError:
-    pass
-
-# Theme options
-#html_theme_options = {
-    # 'typekit_id': 'hiw1hhg',
-    # 'analytics_id': '',
-    # 'sticky_navigation': True  # Set to False to disable the sticky nav while scrolling.
-    #'logo_only': True,  # if we have a html_logo below, this shows /only/ the logo with no title text
-    #'collapse_navigation': False,  # Collapse navigation (False makes it tree-like)
-    # 'display_version': True,  # Display the docs version
-    # 'navigation_depth': 4,  # Depth of the headers shown in the navigation bar
-#}
-
-html_logo = 'images/upbge_logo.png'
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-html_theme_options = {}
-
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = []
 
-if html_theme == "sphinx_rtd_theme":
-    html_theme_options = {
+html_logo = 'images/upbge_logo.png' 
+
+html_title = "UPBGE Manual"
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+
+# Theme options
+html_theme_options = {
         # included in the title
         "display_version": False,
         "collapse_navigation": True,
         "navigation_depth": -1,
         "canonical_url": "https://upbge.org/manual",
     }
-
-    extensions.append('sphinx_rtd_theme')
-
-html_title = "UPBGE Manual"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
